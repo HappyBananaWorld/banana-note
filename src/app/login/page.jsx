f5d96f876d123316340da18409833ef8219c4532
Mr Banana <sajad.dev.personal@gmail.com>
2025-10-03 18:33:02 +0330

    create login form and completed design for editor

---
+++ src/app/login/page.jsx
@@ -0,0 +1,16 @@
+import styles from "@/styles/login/login.module.css";
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles["login-box"]}>
+        <h1>Login</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
